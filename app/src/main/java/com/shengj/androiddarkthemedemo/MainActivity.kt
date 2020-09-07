df4631a704696d70d5ecb7895e2aca279b35de74
shengj <shengj@rd.netease.com>
2020-09-07 17:31:01 +0800

    feat:move action to menu

--- app/src/main/java/com/shengj/androiddarkthemedemo/MainActivity.kt
+++ app/src/main/java/com/shengj/androiddarkthemedemo/MainActivity.kt
@@ -1,51 +1,42 @@
 package com.shengj.androiddarkthemedemo
 
 import android.os.Bundle
-import android.view.View
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO_TIME
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
         setSupportActionBar(toolbar)
-        updateMode()
+        toolbar.overflowIcon = ContextCompat.getDrawable(this, R.drawable.round_more_horiz)
     }
 
-    fun switchLightOrDark(view: View) {
-        when (AppCompatDelegate.getDefaultNightMode()) {
-            MODE_NIGHT_AUTO_TIME,
-            MODE_NIGHT_AUTO_BATTERY,
-            MODE_NIGHT_UNSPECIFIED -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
-            MODE_NIGHT_YES -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
-            MODE_NIGHT_NO -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
-            MODE_NIGHT_FOLLOW_SYSTEM -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_light -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            R.id.action_dark -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            R.id.action_follow -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+            R.id.action_battery -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY)
         }
-        updateMode()
+        return true
     }
 
-    private fun updateMode() {
-        btn_mode.text = getCurrentNightMode()
-    }
-
-    private fun getCurrentNightMode(): String {
-        val mode = "Current Mode: "
-        return when (AppCompatDelegate.getDefaultNightMode()) {
-            MODE_NIGHT_YES -> return mode + "DARK"
-            MODE_NIGHT_NO -> return mode + "LIGHT"
-            MODE_NIGHT_FOLLOW_SYSTEM -> return mode + "FOLLOW SYSTEM"
-            MODE_NIGHT_AUTO_BATTERY -> return mode + "AUTO BATTERY"
-            MODE_NIGHT_UNSPECIFIED -> return mode + "UNSPECIFIED"
-            MODE_NIGHT_AUTO_TIME -> return mode + "AUTO TIME"
-            else -> mode
-        }
-    }
 }
